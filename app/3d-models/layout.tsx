87b7a42b0e339f7c6a06d7a27946d2a12243d815
Mukul Sharma <sharma29212k@gmail.com>
2026-08-13 22:43:23 +0530

    style categories link

--- app/3d-models/layout.tsx
+++ app/3d-models/layout.tsx
@@ -1,34 +1,10 @@
-import { getAllCategories } from "@/app/lib/categories"
-import NavLink from "@/app/components/NavLink"
-import { Category } from "@/app/types"
+import CategoryNav from "../components/CategoryNav"
 
-export default async function CategoryPageLayout({ children }: { children: React.ReactNode }) {
-    const categories: Category[] = getAllCategories()
+export default async function CategoryPageLayout({ children }: { children: React.ReactNode }) {    
     return (
         <div className="relative flex flex-col min-h-screen md:flex-row">
             {/* Responsive Navigation */}
-            <aside className="sticky top-0 z-10 w-full bg-white border-b border-gray-200 md:fixed md:w-64 md:top-1/2 md:-translate-y-1/2 md:border-none">
-                <div className="relative">
-                    <nav className="w-full overflow-x-auto md:overflow-visible scrollbar-hide">
-                        <ul className="flex px-4 py-3 space-x-4 whitespace-nowrap md:flex-col md:p-0 md:space-x-0 md:space-y-3">
-                            <li className="text-sm uppercase">
-                                <NavLink href="/3d-models" >All</NavLink>
-                            </li>
-                            {categories.map(item => (
-                                <li key={item.slug} className="text-sm uppercase">
-                                    <NavLink
-                                        href={`/3d-models/categories/${item.slug}`}
-                                    >
-                                        {item.displayName}
-                                    </NavLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                    {/* Fading edge/gradient for horizontal scroll hint on mobile */}
-                    <div className="absolute top-0 right-0 w-8 h-full pointer-events-none bg-gradient-to-l from-white to-transparent md:hidden" />
-                </div>
-            </aside>
+            <CategoryNav />
 
             {/* Main Content Area */}
             <main className="flex-1 p-4 md:ml-64">{children}</main>
